cf52bae4eaf33410ed6944a90bad91d76633e107
kroune <123553746+kroune@users.noreply.github.com>
2024-12-17 17:02:44 +0300

    added back handle to the web version

--- composeApp/src/wasmJsMain/kotlin/com/kroune/nine_mens_morris_kmp_app/main.kt
+++ composeApp/src/wasmJsMain/kotlin/com/kroune/nine_mens_morris_kmp_app/main.kt
@@ -4,11 +4,18 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import com.arkivanov.decompose.DefaultComponentContext
 import com.arkivanov.essenty.lifecycle.LifecycleRegistry
+import com.kroune.nine_mens_morris_kmp_app.navigation.BackHandler
 import com.kroune.nine_mens_morris_kmp_app.navigation.RootComponent
 import kotlinx.browser.document
+import kotlinx.browser.window
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
+    window.onkeyup = {
+        if (it.key == "Escape") {
+            BackHandler.onCallback()
+        }
+    }
     ComposeViewport(document.body!!) {
         val lifecycle = LifecycleRegistry()
         val component = DefaultComponentContext(lifecycle)
