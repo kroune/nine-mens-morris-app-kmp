c732e3b7b956f92360da83ff6e166127884a27a4
kroune <123553746+kroune@users.noreply.github.com>
2025-05-18 18:48:49 +0300

    work in progress

--- composeApp/src/commonMain/kotlin/io/github/kroune/nine_mens_morris_kmp_app/data/remote/onlineGame/OnlineGameRepositoryI.kt
+++ composeApp/src/commonMain/kotlin/io/github/kroune/nine_mens_morris_kmp_app/data/remote/onlineGame/OnlineGameRepositoryI.kt
@@ -1,10 +1,8 @@
 package io.github.kroune.nine_mens_morris_kmp_app.data.remote.onlineGame
 
-import com.kroune.nineMensMorrisLib.Position
 import com.kroune.nineMensMorrisLib.move.Movement
 import io.github.kroune.nine_mens_morris_kmp_app.model.GiveUpApiResponse
 import io.github.kroune.nine_mens_morris_kmp_app.model.SendMoveApiResponse
-import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.flow.Flow
 
 interface OnlineGameRepositoryI {
