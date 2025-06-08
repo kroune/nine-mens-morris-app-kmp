590f0fb61624ff1eb263ed11af14782593ef1317
kroune <123553746+kroune@users.noreply.github.com>
2025-06-08 16:36:29 +0300

    update

--- baselineprofile/src/main/kotlin/io/github/kroune/baselineprofile/BaselineProfileGenerator.kt
+++ baselineprofile/src/main/kotlin/io/github/kroune/baselineprofile/BaselineProfileGenerator.kt
@@ -58,26 +58,25 @@ class BaselineProfileGenerator {
             startActivityAndWait()
 
             device.waitForIdle()
-            repeat(4) {
-                val start: UiObject2 = device.findObject(By.text("Press to start"))
-                start.click()
-                device.wait(Until.hasObject(By.text("Each circle at the top")), 5000)!!
-                device.waitForIdle()
-                val scrollUpOrDownSelector = By.desc("scroll up or down")
-                device.findObject(scrollUpOrDownSelector).click()
-                device.waitForIdle()
-                val playWithFriendSelector = By.text("Play with friend")
-                device.wait(Until.hasObject(playWithFriendSelector), 5000)!!
-                device.waitForIdle()
-                device.findObject(playWithFriendSelector).click()
-                val analyzeSelector = By.text("Analyze")
-                device.wait(Until.hasObject(analyzeSelector), 5000)!!
-                device.waitForIdle()
-                device.pressBack()
-                device.waitForIdle()
-                device.pressBack()
-                device.waitForIdle()
-            }
+            val start: UiObject2 = device.findObject(By.text("Press to start"))
+            start.click()
+            device.waitForIdle()
+            device.wait(Until.hasObject(By.text("Each circle at the top")), 5000)!!
+            device.waitForIdle()
+            val scrollUpOrDownSelector = By.desc("scroll up or down")
+            device.findObject(scrollUpOrDownSelector).click()
+            device.waitForIdle()
+            val playWithFriendSelector = By.text("Play with friend")
+            device.wait(Until.hasObject(playWithFriendSelector), 5000)!!
+            device.waitForIdle()
+            device.findObject(playWithFriendSelector).click()
+            val analyzeSelector = By.text("Analyze")
+            device.wait(Until.hasObject(analyzeSelector), 5000)!!
+            device.waitForIdle()
+            device.pressBack()
+            device.waitForIdle()
+            device.pressBack()
+            device.waitForIdle()
         }
     }
 }
